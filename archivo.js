0ecd230b76a808a3c1146ecf0a139af4e16ee24b
cristofer0007 <cristofer102.com@gmail.com>
2024-09-11 04:51:53 -0600

    Add files via upload

---
+++ archivo.js
@@ -0,0 +1,11 @@
+$(document).on("ready",inicio);
+function inicio(){
+    $("div").on("click",function(){
+        $("div").css("color","red");
+        $("div").css("background","white");
+        $("div").css("width","400px");
+        $("div").css("height","400px");
+    });
+    
+}
+
